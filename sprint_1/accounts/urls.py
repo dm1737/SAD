182db02ac4b02957f269278fdfe9ab782bc87c9d
Dewonglucas11 <dlucasjr@callallday.com>
2020-01-22 16:47:42 -0500

    Revert "streamlines process, initial landing page goes to login"
    This reverts commit 4911d8404990f7b07b63901d06c24e3a8394d9ba.

--- sprint_1/accounts/urls.py
+++ sprint_1/accounts/urls.py
@@ -6,5 +6,6 @@ from . import views
 #app_name = "users"
 
 urlpatterns = [
-    url(r'^$', LoginView.as_view(template_name='accounts/login.html'), name="login"),
+    url(r'^$', views.home),
+    url('login/', LoginView.as_view(template_name='accounts/login.html'), name="login"),
 ]
