7f05a0b69e7c0f23f932367363d4e48d0d397a94
Kasey Merritt <kmerritt1212@gmail.com>
2020-01-25 01:01:54 -0500

    changing urls to path

--- sprint_1/accounts/urls.py
+++ sprint_1/accounts/urls.py
@@ -1,14 +1,16 @@
-from django.contrib.auth.views import LoginView
-from django.conf.urls import url
-from django.urls import path
-from . import views
+#from django.contrib.auth.views import LoginView
+#from django.conf.urls import url
+#from django.urls import path
+from django.urls import path, include
+#from . import views
+from accounts import views as user_views
 
-
-#app_name = "users"
 
 urlpatterns = [
-    url(r'^$', views.home),
-    url('login/', LoginView.as_view(template_name='accounts/login.html'), name="login"),
+    #url(r'^$', views.home),
+    path('home/', user_views.home, name="home"),
+    #url('login/', LoginView.as_view(template_name='accounts/login.html'), name="login"),
+    path('login/', user_views.login, name="login"),
 ]
 
-
+#path('register/', user_views.register, name='register'),
--- sprint_1/sprint_1/urls.py
+++ sprint_1/sprint_1/urls.py
@@ -1,14 +1,14 @@
-#from django.conf.urls import url, include
+
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
 
 
 urlpatterns = [
-    #url(r'^admin/', admin.site.urls),
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('', include('accounts.urls')),
+
     #url(r'^account/', include('accounts.urls')),
    # url(r'^login/$', login, {'template_name': 'accounts/login.html'})
 ]
