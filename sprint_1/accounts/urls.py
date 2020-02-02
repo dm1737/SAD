7b921e5fd6df35c206d025850c3e2dba2d004190
Kasey Merritt <kmerritt1212@gmail.com>
2020-02-02 14:48:17 -0500

    Styling

--- sprint_1/accounts/urls.py
+++ sprint_1/accounts/urls.py
@@ -7,8 +7,8 @@ app_name = 'accounts'
 urlpatterns = [
     path("", views.homepage, name="homepage"),
     path("register/", views.register, name="register"),
-    path("logout", views.logout_request, name="logout"),
-    path("login", views.login_request, name="login"),
+    path("logout/", views.logout_request, name="logout"),
+    path("login/", views.login_request, name="login"),
     #url(r'^$', LoginView.as_view(template_name='accounts/login.html'), name="home"),
     #url('/home/', LoginView.as_view(template_name='accounts/home.html'), name="home"),
 ]
