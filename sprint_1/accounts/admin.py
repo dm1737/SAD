a8ae79a40d2d41a502e9994cb8f72fe2e119baac
Dewonglucas11 <dlucasjr@callallday.com>
2020-02-16 18:14:54 -0500

    Revert "Merge branch 'Sprint_2-Djr'"
    This reverts commit 5df7e5b1847dfc89963e0f0a373f73af0d7bac16, reversing
    changes made to a41fd927411fc7f6850b6c199faff1129dbd540d.

--- sprint_1/accounts/admin.py
+++ sprint_1/accounts/admin.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import User
-from .models import Tutorial, Post, Profile
+from .models import Tutorial, Post
 from tinymce.widgets import TinyMCE
 from django.db import models
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from .models import Profile
-class UserInline(admin.StackedInline):
-    model = Profile
-    can_delete = False
-    verbose_name_plural = 'user'
-class UserAdmin(BaseUserAdmin):
-    inlines = (UserInline,)
+
 class TutorialAdmin (admin.ModelAdmin):
     
     fields = ["title",
@@ -20,8 +15,6 @@ class TutorialAdmin (admin.ModelAdmin):
         models.TextField: {'widget': TinyMCE()},
         }
 # Register your models here.
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
 admin.site.register(Tutorial, TutorialAdmin)
 admin.site.register(Post)
 
--- sprint_1/accounts/models.py
+++ sprint_1/accounts/models.py
@@ -18,18 +18,6 @@ class Post(models.Model):
     username= models.CharField(max_length=300, unique=True)
     password= models.TextField()
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    attempts = models.IntegerField(default='0')
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
 
 class Profile(models.Model):
     Accountant = 1
