858c68931543f4549414f7b43e37abf6a6963e41
Dewonglucas11 <dlucasjr@callallday.com>
2020-02-15 21:44:25 -0500

    Added password lockout

--- sprint_1/accounts/models.py
+++ sprint_1/accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from datetime import datetime
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 from django.db import models
@@ -21,6 +23,13 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     attempts = models.IntegerField(default='0')
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
                
