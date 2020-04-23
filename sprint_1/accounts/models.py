2e96e3c72e02424a9a65cd534d865769f0955b04
DJ2KO <58568160+DJ2KO@users.noreply.github.com>
2020-04-22 20:56:15 -0400

    Add files via upload

--- sprint_1/accounts/models.py
+++ sprint_1/accounts/models.py
@@ -2,12 +2,15 @@ from django.db import models
 from datetime import datetime
 from datetime import date
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
+from django.db.models import ProtectedError
 from django.dispatch import receiver
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.core.validators import MaxValueValidator, MinValueValidator
 from simple_history.models import HistoricalRecords
-from django.urls import reverse 
+from django.urls import reverse
+from django.contrib import messages
+
 
 
 class Profile(models.Model):
@@ -51,45 +54,71 @@ def is_staff(self):
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-        
+
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
 
-class Statements (models.Model):
-	Total_debit = models.DecimalField(decimal_places=2, max_digits=10)
-	Total_Credit = models.DecimalField(decimal_places=2, max_digits=10)
-	Total_Expense = models.DecimalField(decimal_places=2, max_digits=10)
-	Total_Revenue = models.DecimalField(decimal_places=2, max_digits=10)
-	Net_Profit = models.DecimalField(decimal_places=2, max_digits=10)
-	Divedends = models.DecimalField(decimal_places=2, max_digits=10)
-	Beginning_Balance = models.DecimalField(decimal_places=2, max_digits=10)
-	Ending_Balance = models.DecimalField(decimal_places=2, max_digits=10)
-	Ratio = models.DecimalField(decimal_places=2, max_digits=10)
+class Statement (models.Model):
+    Total_debit = models.DecimalField(decimal_places=2, max_digits=10)
+    Total_Credit = models.DecimalField(decimal_places=2, max_digits=10)
+    Total_Expense = models.DecimalField(decimal_places=2, max_digits=10)
+    Total_Revenue = models.DecimalField(decimal_places=2, max_digits=10)
+    Net_Profit = models.DecimalField(decimal_places=2, max_digits=10)
+    Divedends = models.DecimalField(decimal_places=2, max_digits=10)
+    Beginning_Balance = models.DecimalField(decimal_places=2, max_digits=10)
+    Ending_Balance = models.DecimalField(decimal_places=2, max_digits=10)
 
-class UserAccount (models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class Account (models.Model):
+    Debit = 1
+    Credit = 2
+    Rejected = 3
+    SIDE_CHOICES = (
+        (Debit, 'Debit'),
+        (Credit, 'Credit'),
+        )
+
+    IS = 1
+    BS= 2
+    RE = 3
+    STATEMENT_CHOICES = (
+        (IS, 'IS'),
+        (BS, 'BS'),
+        (RE, 'RE'),   
+        )
     account_name = models.CharField(max_length=300, unique=True)
     account_number = models.PositiveIntegerField(unique=True)
-    account_description = models.TextField()
-    account_category = models.CharField(max_length=300)
-    account_subcategory = models.CharField(max_length=300)
-    initial_balance = models.DecimalField(decimal_places=2, max_digits=10, validators=[MinValueValidator(0)])
-    debit = models.DecimalField(decimal_places=2, max_digits=10)
-    credit = models.DecimalField(decimal_places=2, max_digits=10)
-    balance = models.DecimalField(decimal_places=2, max_digits=10)
+    account_description = models.TextField(null=True, blank=True)
+    normal_side=models.PositiveSmallIntegerField(choices=SIDE_CHOICES, null=False, blank=True, default=Debit)
+    account_category = models.CharField(max_length=300,null=True, blank=True)
+    account_subcategory = models.CharField(max_length=300,null=True, blank=True)
+    initial_balance = models.DecimalField(decimal_places=2, max_digits=10 , default=0,validators=[MinValueValidator(0)])
+    debit = models.DecimalField(decimal_places=2, max_digits=10, default=0)
+    credit = models.DecimalField(decimal_places=2, max_digits=10, default=0)
+    balance = models.DecimalField(decimal_places=2, max_digits=10, default=0)
     account_created = models.DateTimeField(default=datetime.now)
-    order = models.CharField(max_length=300)
-    statement = models.FileField(null=True)
-    comment = models.TextField()
+    user_id = models.CharField(max_length=300, unique=True,null=True, blank=True)
+    order = models.CharField(max_length=300,null=True, blank=True)
+    statement = models.PositiveSmallIntegerField(choices=STATEMENT_CHOICES, null=False, blank=True, default=IS)
+    comment = models.TextField(null=True, blank=True)
     history = HistoricalRecords()
 
+
     def get_absolute_url(self):
-        return reverse('useraccount:detail', args=[self.account_number])
+        return reverse('accounts:detail', args=[self.account_number])
     
+
     def __str__(self):
         return self.account_name
+  
 
+@receiver(pre_delete, sender=Account, dispatch_uid='post_pre_delete_signal')
+def protect_posts(sender, instance, using, **kwargs):
+        if instance.balance == 0: 
+            pass
+        else:  # Any other status types I add later will also be protected
+            
+            raise ProtectedError('Account Still Has a Balance', instance)  
 
 class Journal (models.Model):
     Pending = 1
@@ -109,7 +138,7 @@ class Journal (models.Model):
         (NA, 'NA'),  
     )
     #user = models.ForeignKey(User, related_name='User', null=True, on_delete=models.CASCADE)
-    account = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
     Journal_name = models.CharField(max_length=300, unique=True)
     Journal_number = models.PositiveIntegerField(unique=True)
     Journal_description = models.TextField(null=True, blank=True)
@@ -124,10 +153,9 @@ class Journal (models.Model):
     history = HistoricalRecords()
     Type = models.PositiveSmallIntegerField(choices=TYPE_CHOICES, null=False, blank=True, default=NA)
 
+
     def get_absolute_url(self):
         return reverse('journal:detail', args=[self.Journal_number])
     def __str__(self):
         return self.Journal_name
 
-
-        
